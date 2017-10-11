b07f9ce422434b1f58419df3f1898dfdefc20ff0
Brielle Davis <brielledavis@Brielles-MacBook-Pro.local>
2017-10-10 21:25:51 -0500

    First Commit

---
+++ app/src/main/java/sampleapp/apps/codelife/sampleapp/HomePageTabbedActivity.java
@@ -0,0 +1,264 @@
+package sampleapp.apps.codelife.sampleapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import layout.CameraFragment;
+
+public class HomePageTabbedActivity extends AppCompatActivity implements CameraFragment.OnFragmentInteractionListener{
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private static FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_TAKE_PHOTO);
+                ActivityCompat.requestPermissions(HomePageTabbedActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        //ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        String date = new SimpleDateFormat("yyyy-MM-dd-HH:MM:ss").format(new Date());
+        String outputname = "WIF??IMAGE" + date.toString() + ".jpeg";
+
+
+        File path= Environment.getExternalStorageDirectory();//(Environment.DIRECTORY_DCIM);  //cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File dir= new File(path, "/save/");
+        File file = new File(dir,outputname);
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+        FileOutputStream outputstream = null;
+
+        if(requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK){
+            try{
+                outputstream = new FileOutputStream(file);
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputstream);
+                outputstream.flush();
+                outputstream.close();
+            }
+            catch (Exception e){
+                try {
+                    outputstream.close();
+                } catch (IOException ioe) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_page_tabbed, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            //SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+            FragmentManager fm = getFragmentManager();
+            //SectionsPagerAdapter adapter = new SectionsPagerAdapter(fm);
+            View rootView = null;
+            if(getArguments().getInt(ARG_SECTION_NUMBER) == 1){
+                //fab.hide();
+                rootView = inflater.inflate(R.layout.fragment_home, container, false);
+                //TextView textView = rootView.findViewById(R.id.section_label);
+                //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            }
+            if(getArguments().getInt(ARG_SECTION_NUMBER) == 2){
+                //fab.show();
+                //Fragment frag = null;
+                //frag = new CameraFragment();
+                //FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                //rootView = frag.getView();
+
+
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(intent,REQUEST_TAKE_PHOTO);
+                //rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+                //rootView.
+                //rootView..startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+//                TextView textView = rootView.findViewById(R.id.section_label);
+//                textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            }
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            //Fragment.instantiate(CameraFragment.newInstance("",""));//,R.layout.fragment_camera);
+            //return PlaceholderFragment.newInstance(position + 1);
+
+
+                Fragment frag = null;
+
+                frag = new CameraFragment();
+                try {
+                    return new CameraFragment();
+                }
+                catch (Exception exc){
+                    throw exc;
+                }
+                //return new CameraFragment();
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "FEED";
+                case 1:
+                    return "RECENT";
+//                case 2:
+//                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/sampleapp/apps/codelife/sampleapp/MainActivity.java
@@ -0,0 +1,69 @@
+package sampleapp.apps.codelife.sampleapp;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText _mpassword;
+    private EditText _memailAddress;
+    private ProgressBar _progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupWidgets();
+    }
+
+    private void setupWidgets() {
+        Button sign_in = (Button) findViewById(R.id.signInButton);
+        sign_in.setOnClickListener(this);
+
+        _mpassword = (EditText)findViewById(R.id.PasswordEditText);
+        _memailAddress = (EditText)findViewById(R.id.PasswordEditText);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.signInButton){
+            //valid Email and Password
+            Boolean isValid = ValidateAndAuthenticateUser(_memailAddress,_mpassword);
+            if(isValid){
+                try {
+
+                    startActivity(new Intent(this, HomePageTabbedActivity.class));
+                }
+                catch (Exception exc){
+                    System.err.print(exc);
+                }
+
+            }
+            else
+                Toast.makeText(this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private Boolean ValidateAndAuthenticateUser(EditText memailAddress, EditText mpassword) {
+        //Toast.makeText(this,"Logging In", Toast.LENGTH_SHORT).show();
+        String email = memailAddress.getText().toString().trim();
+        String pass = mpassword.getText().toString().trim();
+
+        //for now everyone is authenticated
+        Toast.makeText(this,"Login Successful", Toast.LENGTH_SHORT).show();
+
+
+        return true;
+    }
+}
